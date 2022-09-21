07591307fdbaef93d8eeb745dad99254d527c552
ICOL-7-2022A\Kyle <kyle@icostore.com>
2022-09-21 15:11:42 -0700

    More code

--- main.py
+++ main.py
@@ -10,34 +10,48 @@ import pandas as pd
 maxTweets = 200
 proVaccineTweets = []
 antiVaxTweets = []
-# finding the provaccine tweets within the last week
+
+# finding the pro-vaccine tweets within the last week
 for i, tweet in enumerate(
         sntwitter.TwitterSearchScraper('#GetVaccinated since:2022-09-14 until:2022-09-21').get_items()):
     if i > maxTweets:
         break
     proVaccineTweets.append([tweet.date, tweet.id, tweet.content, tweet.user.username])
-# Finding the antivaccines tweets withing the last week
+
+# Finding the anti-vaccines tweets withing the last week
 for i, tweet in enumerate(
-        sntwitter.TwitterSearchScraper('#StopVaccination since:2022-09-14 until:2022-09-21').get_items()):
+        sntwitter.TwitterSearchScraper('#antivax since:2022-09-14 until:2022-09-21').get_items()):
     if i > maxTweets:
         break
     antiVaxTweets.append([tweet.date, tweet.id, tweet.content, tweet.user.username])
-# Creating Dataframes for the pro and antivacines
+
+# Creating Dataframes for the pro and anti-vaccines
 proOutput = pd.DataFrame(proVaccineTweets, columns=['Datetime', 'Tweet Id', 'Text', 'Username'])
 antiOutput = pd.DataFrame(antiVaxTweets, columns=['Datetime', 'Tweet Id', 'Text', 'Username'])
 
-# Display first 5 entries from dataframe
-# tweets_df2.head()
+proOutput.head()
+antiOutput.head()
+
+uniqueWords = []
+listWords = []
+iterator = -1
+
+# We are going to create an array of unique words
+for x in proOutput.Text:
+    iterator = iterator + 1
+    listWords.append([])
+    for word in x.split():
+        listWords[iterator].append(word)
+        if uniqueWords.count(word) < 1:
+            uniqueWords.append(word)
+
+# appending a new column with a list of words
+proOutput['Unique'] = listWords
 
 # Exporting the panda dataframes into JSON
-proOutput.to_json('proVaxJSON.json', orient='index')
-antiOutput.to_json('antiVaxJSON.json', orient='index')
+hey = proOutput.to_json('proVaxJSON.json', orient='index')
+ayo = antiOutput.to_json('antiVaxJSON.json', orient='index')
 
-# Export dataframe into a CSV
-# proOutput.to_csv('proVax.csv', sep=',', index=False)
-# antiOutput.to_csv('antiVax.csv', sep=',', index=False)
-
-g1 = nx.petersen_graph()
-nx.draw(g1)
-plt.show()
-
+# g = nx.from_pandas_dataframe(proOutput, source='Username', target='Text')
+# nx.draw(g)
+# plt.show()
