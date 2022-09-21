ff56984aac915248091cc1ff0aa3f599a8be72a8
ICOL-7-2022A\Kyle <kyle@icostore.com>
2022-09-21 12:28:38 -0700

    adding JSON output file for vax and antivax

--- main.py
+++ main.py
@@ -7,25 +7,35 @@ import snscrape.modules.twitter as sntwitter
 import pandas as pd
 
 # Creating global variables for the amount of tweets I want to pull, and also two arrays for the tweets
-maxTweets = 100
+maxTweets = 200
 proVaccineTweets = []
 antiVaxTweets = []
-
-# Using TwitterSearchScraper to scrape data and append tweets to list
+# finding the provaccine tweets within the last week
 for i, tweet in enumerate(
-        sntwitter.TwitterSearchScraper('#covidvaccines since:2022-09-14 until:2022-09-21').get_items()):
+        sntwitter.TwitterSearchScraper('#GetVaccinated since:2022-09-14 until:2022-09-21').get_items()):
     if i > maxTweets:
         break
     proVaccineTweets.append([tweet.date, tweet.id, tweet.content, tweet.user.username])
-
-# Creating a dataframe from the tweets list above
-tweets_df2 = pd.DataFrame(proVaccineTweets, columns=['Datetime', 'Tweet Id', 'Text', 'Username'])
+# Finding the antivaccines tweets withing the last week
+for i, tweet in enumerate(
+        sntwitter.TwitterSearchScraper('#StopVaccination since:2022-09-14 until:2022-09-21').get_items()):
+    if i > maxTweets:
+        break
+    antiVaxTweets.append([tweet.date, tweet.id, tweet.content, tweet.user.username])
+# Creating Dataframes for the pro and antivacines
+proOutput = pd.DataFrame(proVaccineTweets, columns=['Datetime', 'Tweet Id', 'Text', 'Username'])
+antiOutput = pd.DataFrame(antiVaxTweets, columns=['Datetime', 'Tweet Id', 'Text', 'Username'])
 
 # Display first 5 entries from dataframe
 # tweets_df2.head()
 
+# Exporting the panda dataframes into JSON
+proOutput.to_json('proVaxJSON.json', orient='index')
+antiOutput.to_json('antiVaxJSON.json', orient='index')
+
 # Export dataframe into a CSV
-tweets_df2.to_csv('proVax.csv', sep=',', index=False)
+# proOutput.to_csv('proVax.csv', sep=',', index=False)
+# antiOutput.to_csv('antiVax.csv', sep=',', index=False)
 
 g1 = nx.petersen_graph()
 nx.draw(g1)
