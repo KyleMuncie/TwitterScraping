0601e4d50cbd38f7e536feec5d048341fb87d956
ICOL-7-2022A\Kyle <kyle@icostore.com>
2022-09-22 13:27:24 -0700

    Final code commit.

--- main.py
+++ main.py
@@ -27,10 +27,13 @@ def kyle():
             break
         antiVaxTweets.append(tweet.content)
 
+    # initiating a set to hold unique words
+    # initiating arrays to hold the edges
     uniqueProWords = set()
     uniqueAntiWords = set()
     proEdges = []
     antiEdges = []
+    # exclusion pattern to not accept any https which are gifs or any @ signs or most emojis
     exclusionPattern = re.compile(r'(^https|[\U00010000-\U0010ffff]|^@|\U0000231B)', flags=re.UNICODE)
 
     # node vals
@@ -50,6 +53,7 @@ def kyle():
             if i < len(words) - 1 and len(words) > 1:
                 proEdges.append((words[i], words[i + 1]))
 
+    # Same as the previous but with is for antiVax
     for tweet in antiVaxTweets:
         rawWords = tweet.split()
         words = []
@@ -63,10 +67,27 @@ def kyle():
             if i < len(words) - 1 and len(words) > 1:
                 antiEdges.append((words[i], words[i + 1]))
 
-    string_list = ["#GetVaccinated", "#antivax", "the", "to", 'to']
+    # This function is for page rank
+    def plot_pagerank_dist(graph, fileName):
+        pagerank_list = list(nx.pagerank(graph).values())
+        plt.hist(pagerank_list, alpha=0.85)
+        plt.xlabel('Degree')
+        plt.ylabel('Frequency')
+        plt.title('Histogram for Page Rank')
+        plt.savefig(fileName + ".png")
 
+    # this function is for clustering
+    def plot_clustering_dist(graph, fileName):
+        clustering_coeffs_list = list(nx.clustering(graph).values())
+        plt.hist(clustering_coeffs_list, bins=100, range=(0.0, 1.0), density=False)
+        plt.xlabel('Degree')
+        plt.ylabel('Frequency')
+        plt.title('Histogram for Clustering')
+        plt.savefig(fileName + ".png")
+
+    # This function is for degree distribution
     def plot_degree_dist(importedGraph, fileName):
-        degrees = [importedGraph.degree(n) for n in importedGraph.nodes() if n not in string_list]
+        degrees = [importedGraph.degree(n) for n in importedGraph.nodes()]
         plt.hist(degrees, bins=50, rwidth=20)
         plt.xlabel('Degree')
         plt.ylabel('Frequency')
@@ -75,13 +96,24 @@ def kyle():
 
     # Creating a graph and then saving it into a png
     g = nx.Graph()
+    # adding the nodes from the unique words for pro-vaccine
     g.add_nodes_from(list(uniqueProWords))
+    # adding the edges
     g.add_edges_from(proEdges)
+    # drawing the graph
     nx.draw(g, with_labels=True, node_size=100)
+    # styling the graph
     plt.title("Network Graph for Pro-Vaccine")
+    # Saving the figure as a png
     plt.savefig("networkGraphProVaccine.png")
+    # clearing the plt
     plt.clf()
+    # making three calls to functions and passing the graph to make histograms
     plot_degree_dist(g, "proVaxHistogram")
+    plt.clf()
+    plot_clustering_dist(g, "proClusteringHistogram")
+    plt.clf()
+    plot_pagerank_dist(g, "proPageRankHistogram")
 
     # clearing the graph and then also clearing the plt
     g.clear()
@@ -96,9 +128,10 @@ def kyle():
     plt.savefig("networkGraphAntiVaccine.png")
     plt.clf()
     plot_degree_dist(g, "antiVaxHistogram")
-
-    g.clear()
     plt.clf()
+    plot_clustering_dist(g, "antiClusteringHistogram")
+    plt.clf()
+    plot_pagerank_dist(g, "antiPageRankHistogram")
 
 
 if __name__ == "__main__":
